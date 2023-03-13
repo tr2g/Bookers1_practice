8e984b0d240aa95a12ff864442b673802b3c05a9
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-03-13 06:22:10 +0000

    [edit]book詳細画面,ページ下部に編集リンクと戻るリンクを設置

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,15 +15,21 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update
+    redirect_to books_path
   end
 
   def destroy
+    @book = Book.find(params[:id])
   end
 
   private
