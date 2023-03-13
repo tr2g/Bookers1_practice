481e70cc5df4a4bbdf4f27e89c84a04bdcf8e47f
EC2 Default User <ec2-user@ip-172-31-4-95.ap-northeast-1.compute.internal>
2023-03-13 03:41:05 +0000

    [edit]books_controller投稿機能実装中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
   end
 
   def index
@@ -21,5 +25,10 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+   def book_params
+     params.require(:book).permit(:title, :body)
+   end
 
 end
